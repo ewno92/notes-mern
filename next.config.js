7a1cf58e5c523bcf2a58daccea0fa7737f2fdac0
ewno92 <77653755+ewno92@users.noreply.github.com>
2021-07-08 16:14:44 -0700

    backend setup complete

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  // reactStrictMode: true,
+  env: {
+    MONGO_URI:
+      "mongodb+srv://test:test123@social-network.jkjo0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  },
+};
